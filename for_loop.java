c0fdbff9d201d7744af874327efb1ad8868bdcc0
Jaydeep Singh Chouhan <jaydeepchouhan20808@acropolis.in>
2022-06-21 17:15:47 +0530

    forr_loop.java added

---
+++ for_loop.java
@@ -0,0 +1,12 @@
+package com.git.main;
+
+public class for_loop {
+
+	public static void main(String[] args) {
+		for(int i =1; i<=10; i++) {
+			System.out.println(5+ "*"+i+"="+5*i);
+		}
+
+	}
+
+}
