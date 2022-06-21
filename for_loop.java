9c683816551fc41baa47f815062fce1d44b8adbc
Jaydeep Singh Chouhan <jaydeepchouhan20808@acropolis.in>
2022-06-21 17:22:42 +0530

    completed for loops with while loops

--- for_loop.java
+++ for_loop.java
@@ -6,7 +6,8 @@ public class for_loop {
 		for(int i =1; i<=10; i++) {
 			System.out.println(5+ "*"+i+"="+5*i);
 		}
-
+		System.out.println("Done with loops");
+		System.out.println("Wait what about while loop");
 	}
 
 }
